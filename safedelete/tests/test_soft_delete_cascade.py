a321c23e02c293812b60b0fdd93e142ca5a6d9f0
Adam Jacquier-Parr <aljparr0@gmail.com>
2018-05-17 15:39:33 +1000

    Added a simple test case for filtering using the in lookup and a SafeDeleteQueryset

--- safedelete/tests/test_soft_delete_cascade.py
+++ safedelete/tests/test_soft_delete_cascade.py
@@ -1,6 +1,7 @@
+import unittest
 from django.db import models
 from django.test import TestCase
-from safedelete import SOFT_DELETE_CASCADE
+from safedelete import SOFT_DELETE_CASCADE, SOFT_DELETE
 from safedelete.models import SafeDeleteModel
 from safedelete.tests.models import Article, Author, Category
 
@@ -16,7 +17,6 @@ class PressNormalModel(models.Model):
 
 
 class CustomAbstractModel(SafeDeleteModel):
-
     class Meta:
         abstract = True
 
@@ -30,7 +30,6 @@ class ArticleView(CustomAbstractModel):
 class SimpleTest(TestCase):
 
     def setUp(self):
-
         self.authors = (
             Author.objects.create(),
             Author.objects.create(),
@@ -98,3 +97,41 @@ class SimpleTest(TestCase):
         self.assertEqual(Article.objects.count(), 3)
         self.assertEqual(Category.objects.count(), 3)
         self.assertEqual(Press.objects.count(), 1)
+
+
+class SafeDeleteWithInOp(TestCase):
+
+    def setUp(self):
+        self.authors = (
+            Author.objects.create(),
+            Author.objects.create(),
+            Author.objects.create(),
+        )
+
+        self.articles = (
+            Article.objects.create(author=self.authors[1]),
+            Article.objects.create(author=self.authors[1]),
+            Article.objects.create(author=self.authors[2]),
+        )
+
+        self.press = (
+            Press.objects.create(name='press 0', article=self.articles[0]),
+            Press.objects.create(name='press 1', article=self.articles[1]),
+            Press.objects.create(name='press 2', article=self.articles[2]),
+        )
+
+    def test_filter_with_in(self):
+        """
+        Issue https://github.com/makinacorpus/django-safedelete/issues/101
+
+        Using a SafeDeleteQueryset as the argument to a filters `in` lookup only works if the queryset is
+          evaluated. i.e. Press.objects.filter(article__in=list(articles))
+        """
+        self.articles[0].delete(force_policy=SOFT_DELETE)
+        self.articles[1].delete(force_policy=SOFT_DELETE)
+
+        articles = Article.objects.filter()
+        #  press = Press.objects.filter(article__in=articles)  # This doesn't work but should
+        press = Press.objects.filter(article__in=list(articles))  # This works
+
+        self.assertEqual(press.count(), 1)
